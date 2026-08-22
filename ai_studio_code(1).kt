ed20a0fa3d8809c46682299829dbe833c9a7567a
Didwagh <157954822+Didwagh@users.noreply.github.com>
2026-08-22 16:57:29 +0530

    Add files via upload

---
+++ ai_studio_code(1).kt
@@ -0,0 +1,20 @@
+package com.example
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class ExamplePlugin : Plugin() {
+
+    override fun load(context: Context) {
+        // Register the main API
+        registerMainAPI(ExampleProvider())
+    }
+
+    companion object {
+        const val PREF_HOST = "companion_host"
+        const val PREF_CHANNEL_ID = "telegram_channel_id"
+    }
+}
